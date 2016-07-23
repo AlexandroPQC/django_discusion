1e6796c608fbd42df8ea351b205c607a444c8d8a
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2016-07-23 19:45:47 -0400

    Conexión con Postgres configurada

--- SistemaDiscusiones/SistemaDiscusiones/settings/local.py
+++ SistemaDiscusiones/SistemaDiscusiones/settings/local.py
@@ -6,8 +6,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Discusiones',
+        'USER': 'alexandro',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
