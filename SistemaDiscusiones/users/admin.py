f7143a71c860d55c7f259152f39ba58caec78bb2
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2016-07-24 21:44:12 -0400

    Admin configurado para el modelo User.

--- SistemaDiscusiones/users/admin.py
+++ SistemaDiscusiones/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        ('User', {'fields' : ('username', 'password')}),
+        ('Personal Information', {'fields': ('first_name',
+                                            'last_name',
+                                            'email',
+                                            'avatar')}),
+        ('Permissions', {'fields': ('is_active',
+                                    'is_staff',
+                                    'is_superuser',
+                                    'groups',
+                                    'user_permissions')}),
+    )
+
+admin.site.register(User, UserAdmin)
